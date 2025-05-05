12e833adbcc3b909e9b6db3018da0109d44a7db2
Alina Zhdanova <gdanova98@gmail.com>
2025-05-06 00:42:04 +0700

    Step 5 - Nested validation. 1. Added the use of additional constraints to check the values of the Map object. 2. Tests have been written.

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -22,7 +22,7 @@ public class NumberSchema extends BaseSchema<Integer> {
     }
 
     public NumberSchema positive() {
-        nameCheckToMechanicsCheck.put("positive", value -> value > 0);
+        nameCheckToMechanicsCheck.put("positive", value -> value == null || value > 0);
         return this;
     }
 
